aa095173b0a8d9b9b0e9992f2dcde4c84599a323
Razzak17 <81388096+Razzak17@users.noreply.github.com>
2022-12-05 14:42:19 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-
+  str = str.toLowerCase();
+    console.log(str)
+    for(let i = 0; i < str.length/2; i++){
+        if(str[i] != str[str.length - 1-i])
+        return false;
+    }
+    return true;
 }
 module.exports = palindrome
